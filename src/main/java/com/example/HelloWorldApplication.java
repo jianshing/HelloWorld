cec5b97fa22fa87d8f4d3196a1f79486123cec81
Tester <Tester@Tester-PC.fios-router.home>
2015-11-19 17:12:02 -0500

    Second Commit

--- src/main/java/com/example/HelloWorldApplication.java
+++ src/main/java/com/example/HelloWorldApplication.java
@@ -2,6 +2,7 @@ package com.example;
 /*
  * Add Author
  * Author : JW
+ * Second commit
  */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
